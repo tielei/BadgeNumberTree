30bfad08c3e4d7ff06c99e85df16b2a8335c18c6
Charles Zhang <charleszhang@welove-inc.com>
2016-06-29 12:23:02 +0800

    layout已全部准备好

--- AndroidDemos/app/src/main/java/com/zhangtielei/demos/badge_number/tabs/SecondFragment.java
+++ AndroidDemos/app/src/main/java/com/zhangtielei/demos/badge_number/tabs/SecondFragment.java
@@ -16,12 +16,16 @@
 
 package com.zhangtielei.demos.badge_number.tabs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.zhangtielei.demos.badge_number.R;
+import com.zhangtielei.demos.badge_number.msg.CommentActivity;
+import com.zhangtielei.demos.badge_number.msg.LikedActivity;
+import com.zhangtielei.demos.badge_number.msg.SysMsgActivity;
 
 /**
  * Created by charleszhang on 2/5/16.
@@ -31,5 +35,32 @@ public class SecondFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.second_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(final Bundle instance) {
+        super.onActivityCreated(instance);
+
+        getView().findViewById(R.id.comment_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), CommentActivity.class));
+            }
+        });
+
+        getView().findViewById(R.id.liked_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), LikedActivity.class));
+            }
+        });
+
+        getView().findViewById(R.id.sysmsg_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), SysMsgActivity.class));
+            }
+        });
+
     }
 }
